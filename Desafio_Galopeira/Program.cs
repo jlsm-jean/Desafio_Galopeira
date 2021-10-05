c4ad5878deed006f21a288c457d94693e1b70798
Jean Leonardo Soares de Moraes <jean.2018130570@unicap.br>
2021-10-05 10:22:18 -0300

    Add project files.

---
+++ Desafio_Galopeira/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Teste
+{
+
+	static void Main(string[] args)
+	{
+		int tst;
+		int lnth;
+		string name = new string(new char[10100]);
+		string tempVar = Console.ReadLine();
+		if (tempVar != null)
+		{
+			tst = int.Parse(tempVar);
+
+
+			while ((tst--) != 0)
+			{
+				string[] tempVar2 = Console.ReadLine().Split();
+				if (tempVar2 != null)
+				{
+					name = tempVar2[0];
+				}
+				lnth = name.Length;
+				Console.Write("{0:f2}\n", lnth * .01);
+			}
+		}
+	}
+
+}
